030b15e2b9f36807116f888a14f66fbf05aebea4
Jonah <jonahdevelopers@gmail.com>
2024-08-18 20:13:38 +0100

    Add event listener to the squares to paint when hover

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ for (let i = 1; i <= 256; i++) {
     const square = document.createElement("div");
     square.style.width = "60px";
     square.style.height = "60px";
+    square.addEventListener("mouseover", () => {
+        square.classList.add("hover");
+    })
     container.appendChild(square)
 }
